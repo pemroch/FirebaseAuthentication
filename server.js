edf2caa6f5b606e8c11a2ab827ad1d3c5f5de62b
pemroch <pemroch@gmail.com>
2016-04-06 17:00:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 var fire = require( 'firebase' );
-var firebaseRef = new Firebase( 'https://githubsamples.firebaseio.com/' );
+var firebaseRef = new Firebase( /* your firebase url */ );
 var express = require( 'express' );
 var app = express();
 var port = process.env.PORT || 8080;
 
-//Configuration
+// Configuration
 app.configure( function() {
   app.use( express.static( __dirname + '/public' ) );
   app.use( express.bodyParser() ); 							
 });
 
-//Routes
+// Routes
 app.post( '/api/createAccount', function( req, res ) {
+  // creates a user with email and password requested from from client from /public/App/UI/CreateAccount/createAccount.js
   firebaseRef.createUser({
     email: req.body.email,
     password: req.body.password
   }, function( error, userData ) {
     if ( error ) {
+      // if unsuccessful, an error message will be returned to be displayed to the client.
       res.json({ error: error.message });
     } else {
+      // if successful, a success object is retuned to the client and a new entry 
+      // with the company name is added to the database under a user specific UID.
       firebaseRef.child( userData.uid ).child( 'company' ).set( req.body.company );
       res.json({ success: true });
     }
@@ -26,9 +30,11 @@ app.post( '/api/createAccount', function( req, res ) {
 });
 
 app.post( '/api/adminReady', function( req, res ) {
+  // location of the user specific information ( will contain the company name set when the user was created )
   var access = firebaseRef.child( req.body.data )
   access.once( 'value', function( dataSnapshot ) {
-    res.json({ success: true, uid: req.body.data, company: dataSnapshot.val().company });
+    // once this information is accessed an object is returned to the client with the data ( company name )
+    res.json({ success: true, company: dataSnapshot.val().company });
   });  
 });
 
