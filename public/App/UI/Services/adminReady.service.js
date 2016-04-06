955832e16043b27d1b2341d46306fdb159a8dfcd
pemroch <pemroch@gmail.com>
2016-04-06 17:09:34 -0400

    Update adminReady.service.js

--- public/App/UI/Services/adminReady.service.js
+++ public/App/UI/Services/adminReady.service.js
@@ -17,15 +17,24 @@
 
       function adminReadyService() { 
         var deferred = $q.defer();
+        // Once logged in the user object is requested
         var authData = $rootScope.firebaseRef.getAuth();
         if ( authData ) {
+          // If the user is logged in, the UID in the user object will be used 
+          // to access user specific data from the server
           $http.post( '/api/adminReady', { data: authData.uid } )
             .success( function( result ) {
-            $rootScope.uid = result.uid;
+            // if successful global values are set from values returned from the server in server.js file
+            // A global value for uid is set to the user's UID
+            $rootScope.uid = authData.uid;
+            // A global value for the company name is set to the company name set
+            // when the user was created.
             $rootScope.company = result.company;            
             deferred.resolve();
           })
             .error( function( err ) {
+            // If unsuccessful, the promise will be rejected and a stateChange will be handled
+            // and the user will be redirected to the login page ( stateChange handled from /public/App/app.run.js )
             deferred.reject();
           });          
           return deferred.promise;
