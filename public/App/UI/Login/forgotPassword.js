f8b9614d1c2bf70782a864ef0d283f6687a8d5e9
pemroch <pemroch@gmail.com>
2016-04-06 17:29:58 -0400

    Update forgotPassword.js

--- public/App/UI/Login/forgotPassword.js
+++ public/App/UI/Login/forgotPassword.js
@@ -25,12 +25,15 @@
     }      
     function submit() {
       if( vm.form.$valid ) {
-        var ref = $rootScope.firebaseAuth;
+        var ref = $rootScope.firebaseAuth
+        // Request a password reset from firebase using the user's email address.
         ref.$resetPassword({
           email: vm.email
         }).then( function() {
+          // If successful, a success message is displayed to the user.
           vm.success = true;
         }).catch( function( error ) {
+          // If unsuccessful, an error message is displayed to the user.
           switch ( error.code ) {
             case "INVALID_USER":
               vm.error = 'The specified user account does not exist.';
