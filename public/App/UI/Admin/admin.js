eb1dea333fb0b2cd3b15c8090b081fc536f05a86
pemroch <pemroch@gmail.com>
2016-04-06 17:17:06 -0400

    Update admin.js

--- public/App/UI/Admin/admin.js
+++ public/App/UI/Admin/admin.js
@@ -14,11 +14,14 @@
     
     var vm = this;
     
+    // Displays global object set from user specific values returned from the server.
     vm.company = $rootScope.company;
     vm.uid = $rootScope.uid    
+    
     vm.logOut = logOut;
     
     function logOut() {
+      // Returns the user to the login page
       $state.go( 'login' );
     }
     
