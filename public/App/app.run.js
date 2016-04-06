71bad0a0916a90daac9d4651eb73373383dc1fb9
pemroch <pemroch@gmail.com>
2016-04-06 17:14:24 -0400

    Update app.run.js

--- public/App/app.run.js
+++ public/App/app.run.js
@@ -12,12 +12,14 @@
   ];
   
   function RunBlock( $firebaseAuth, $rootScope, $state ) {  
+    // Global objects used throughout the application.
     $rootScope.uid = '';
     $rootScope.company = '';
-    $rootScope.firebaseRef = new Firebase( 'https://githubsamples.firebaseio.com/' );
+    $rootScope.firebaseRef = new Firebase( /* your firebase url */ );
     $rootScope.firebaseAuth = $firebaseAuth( $rootScope.firebaseRef );
     $rootScope.$on( "$stateChangeError", function( event, toState, toParams, fromState, fromParams, error ) {
       if (error === "AUTH_REQUIRED") {
+        // stateChanged error used to redirect a user to the login screen if the user is no logged in.
         $state.go( 'login' );
       }    
     });
